a0c4e93743bec3cad47147a9b7c952c256ab0b12
Jaco Kleynhans <jaco@ninjaco.co.za>
2021-06-06 22:12:53 +0200

    USB Reader Library

---
+++ UnoLib/UnoLib/MyCustomControl.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Windows.UI.Xaml.Controls;
+
+namespace UnoLib
+{
+    public partial class MyCustomControl : UserControl
+    {
+        public MyCustomControl()
+        {
+        }
+    }
+}
